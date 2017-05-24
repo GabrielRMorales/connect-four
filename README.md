2d100f0c91142a96feac4372ff6805cdc07d184e
Gabriel R Morales <grmorale@gmail.com>
2017-05-24 16:40:15 -0700

    Create Connect Four prototype based on basic rspec tests

---
+++ README.md
@@ -0,0 +1,2 @@
+The classic Connect Four game.
+This version is designed through TDD. The board is 4x4. Players either use 1 or 0.
---
+++ spec/connectfour_spec.rb
@@ -0,0 +1,83 @@
+require "connectfour.rb"
+
+describe ConnectFour do 
+
+	let(:game) { ConnectFour.new }
+
+	describe "#set_board" do
+		it "sets a 4x4 board" do
+			expect(game.set_board).to eql([["_", "_", "_", "_"],["_", "_", "_", "_"],
+									  ["_", "_", "_", "_"],["_", "_", "_", "_"]])
+		end
+	end
+
+	describe "#show_moves" do
+
+		it "shows the available moves" do
+			game.instance_variable_set :@board, [["_", "_", "_", "_"],["_", "_", "_", "_"],
+									  ["_", "_", "_", "_"],["_", "_", "_", "_"]]
+			expect{game.show_moves}.to output("You may choose:\n0\n1\n2\n3\n").to_stdout
+
+		end
+
+		it "only shows the available lanes" do
+			game.instance_variable_set :@board, [["1", "1", "1", "1"],["_", "_", "_", "_"],
+									  ["0", "0", "0", "0"],["_", "_", "_", "_"]]
+			expect{game.show_moves}.to output("You may choose:\n1\n3\n").to_stdout
+		end
+
+		#make this last test check if all the slots are NOT "_"--it should return gameover
+		#it "only shows the available lanes" do
+		#	game.instance_variable_set :@board, [["1", "1", "1", "1"],["_", "_", "_", "_"],
+		#							  ["0", "0", "0", "0"],["_", "_", "_", "_"]]
+		#	expect{game.show_moves}.to output("You may choose:\n1\n3\n").to_stdout
+		#end
+
+	end
+
+	describe "#set_symbol" do
+			it "sets the @sym based on the turn number" do
+	          game.instance_variable_set :@counter, 2
+			  expect(game.set_symbol).to eql("1")
+		    end	
+
+		    it "sets the @sym based on the turn number" do
+	          game.instance_variable_set :@counter, 3
+			  expect(game.set_symbol).to eql("0")
+		    end	
+	end
+
+	describe "#check_move" do
+		it "checks if user's choice is valid" do
+			game.instance_variable_set :@user_options, [1]
+			game.instance_variable_set :@user_choice, 1
+			expect(game.check_move).to eql("valid")
+		end
+	end
+
+	describe "#set_move" do
+		it "changes board to represent user choice" do			
+			game.instance_variable_set :@counter, 2
+			game.set_symbol
+			game.instance_variable_set :@user_choice, 2
+			expect(game.set_move).to eql([["_", "_", "_", "_"],["_", "_", "_", "_"],
+									  ["1", "_", "_", "_"],["_", "_", "_", "_"]])
+		end
+	end
+
+	describe "#draw" do
+		it "shows the current board" do
+			game.instance_variable_set :@board, [["_", "_", "_", "_"],["_", "_", "_", "_"],
+									  ["_", "_", "_", "_"],["_", "_", "_", "_"]]
+			expect{game.draw}.to output("Board:\n____\n____\n____\n____\n").to_stdout
+		end
+	end
+	
+	
+
+	describe "#gameplay" do
+		it "continues for 16 turns" do
+			expect(game.gameplay).to eql(true)
+		end
+	end
+end
